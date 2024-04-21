a2ca2bc61bd2120c4005cedfaaa9a9d7e35b6312
Victor-droid165 <victor.emanuel.barbosa.rodrigues@ccc.ufcg.edu.br>
2024-04-21 17:36:37 -0300

    fix: Clearing data in database

--- backend/database/users.pl
+++ backend/database/users.pl
@@ -1,14 +1,13 @@
 :- dynamic user/6.
 
 user(1, "Everton", "everton@admin.ufcg.edu.br", "senhasegura", "administrator", "19-04-2024 08:00:00").
-user(2, "John Doe", "everton@admin.ufcg.edu.br", "password13", "student", "20-04-2024 12:00:00").
 
 :- dynamic user/8.
 
 
 :- dynamic current_user_id/1.
 
-current_user_id(2).
+current_user_id(1).
 
 next_user_id(A) :-
     current_user_id(B),
