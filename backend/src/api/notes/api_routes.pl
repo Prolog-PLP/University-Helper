294e213cef9a6ec83ec10da22b8bfea8083c9cf3
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2024-04-24 23:02:55 -0300

    feat: inserting notes routes
    Co-authored-by: WendellTMO <wendelltmo2306@outlook.com>

---
+++ backend/src/api/notes/api_routes.pl
@@ -0,0 +1,12 @@
+:- consult('api_functions.pl').
+
+http:location(notes, api(notes), []).
+
+:- http_handler(notes(.), get_notes_handler, [method(get)]).
+# Fazer o match com o 'objeto' ou apenas o ID
+:- http_handler(notes(delete), delete_notes_handler, [method(delete)]).
+:- http_handler(notes(add), add_notes_handler, [method(post)]).
+:- http_handler(notes(update), update_notes_handler, [method(post)]).
+:- http_handler(notes(getId), get_note_id_handler, [method(post)]).
+:- http_handler(notes(notifyUser), notify_user_handler, [method(post)]).
+:- http_handler(notes(userNotifications), user_notifications_handler, [method(get)])
