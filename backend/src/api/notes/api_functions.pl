2d643a3dc02a10ccef1a8deb8f175e086aaf0e2b
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2024-04-25 00:28:57 -0300

    Revert "feat: first implementation of notes"
    This reverts commit 49b80dc9375bb10d70db41641c1ab38b6de75edc.

--- backend/src/api/notes/api_functions.pl
+++ backend/src/api/notes/api_functions.pl
@@ -1,6 +1,1 @@
 :- consult('../../controllers/note_controller.pl').
-
-add_note_handler(Request) :-
-    http_read_json_dict(Request, Note),
-    add_note(Note, Response),
-    reply_json(Response).
