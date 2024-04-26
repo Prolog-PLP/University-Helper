a4acc950acdbbc630540c19e769be3e30518e219
KaykyFidelis <kayky.serafim@lsd.ufcg.edu.br>
2024-04-25 23:10:19 -0300

    remove: unused route getID
    Co-authored-by: WendellTMO <wendelltmo2306@outlook.com>

--- backend/src/api/notes/api_routes.pl
+++ backend/src/api/notes/api_routes.pl
@@ -7,6 +7,5 @@ http:location(notes, api(notes), []).
 :- http_handler(notes(delete), delete_notes_handler, [method(delete)]).
 :- http_handler(notes(add), add_note_handler, [method(post)]).
 :- http_handler(notes(update), update_notes_handler, [method(post)]).
-:- http_handler(notes(getId), get_note_id_handler, [method(post)]).
 :- http_handler(notes(notifyUser), notify_user_handler, [method(post)]).
 :- http_handler(notes(userNotifications), user_notifications_handler, [method(get)]).
